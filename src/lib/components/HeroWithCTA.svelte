8ac1d682f121c5acbb4e57c69c94817117cf8e64
Artifex <markasena@gmail.com>
2023-09-17 11:34:31 +0800

    🐞 bug: Index overloads buttons
    #5 Hero Section
    
    Add temporarry asset, idk how to download on figma lol!
    Add Multiple Hero Sections
    Wrong indexes gradient on gradient fills

---
+++ src/lib/components/HeroWithCTA.svelte
@@ -0,0 +1,42 @@
+<script>
+	import SectionHeader from './sections/SectionHeader.svelte';
+</script>
+
+<div class="relative isolate pt-2">
+	<div
+		class="absolute inset-x-0 -top-40 -z-20 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+		aria-hidden="true"
+	>
+		<div
+			class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#f72585] to-[#7209b7] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+		/>
+	</div>
+	<div
+		class="py-12 sm:py-32 lg:pb-40 bg-contain bg-no-repeat bg-center"
+		style="background-image: url('/smokes.jpg');"
+	>
+		<div class="mx-auto max-w-7xl px-6 lg:px-8">
+			<div class="mx-auto max-w-2xl text-center">
+				<h1 class="text-8xl dark:text-white text-white font-black antialiased tracking-tighter">
+					carry the world with our
+					<div class="inline-flex justify-center bg-purple-500 rounded-xl">
+						<a href="/contact" class="text-black text-2xl font-light tracking-normal px-4 py-2"
+							>Contact Us</a
+						>
+					</div>
+					craftsmanship
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div
+		class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+		aria-hidden="true"
+	>
+		<div
+			class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#f72585] to-[#7209b7] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+		/>
+	</div>
+</div>
