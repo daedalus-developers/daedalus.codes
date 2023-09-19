83cf036deff85b5590a4cd896080af7870da63e4
DenverBrian <denver.talamayan@gmail.com>
2023-09-20 05:38:36 +0800

    add  card border

--- src/lib/components/sections/SectionHeaderCourses.svelte
+++ src/lib/components/sections/SectionHeaderCourses.svelte
@@ -4,20 +4,16 @@
 </script>
 
 <div class="mx-auto max-w-7xl px-6 lg:px-8">
-	<div class="mx-auto text-center flex justify-center ">
-		<h2 style='color:#8D62EF;'
+	<div class="mx-auto text-center flex justify-center">
+		<h2
+			style="color:#8D62EF;"
 			class="text-sm font-bold basis-1/4 text-left tracking-tight
-			 dark:text-white  "
+			 dark:text-white"
 		>
 			{title}
 		</h2>
-		<p
-			class="font-bold text-left tracking-tight text-gray-900 text-4xl dark:text-white  "
-		>
+		<p class=" font-bold text-left tracking-tight text-gray-900 text-4xl dark:text-white">
 			{@html description}
-			
 		</p>
-		
 	</div>
 </div>
-
