348d4f6265abeb268460c19f265fa2bf2cca82f3
Artifex <markasena@gmail.com>
2023-09-21 07:43:36 +0800

    ✨feat: Megaphone
    Message needs to be from our backend

---
+++ src/lib/components/Megaphone.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let announcement: string;
+</script>
+
+{#if announcement}
+	<div class="mx-auto flex w-full bg-[#8d62ef]">
+		<div class="mx-auto"><h1>Courses now On Sale 💰</h1></div>
+	</div>
+{/if}
