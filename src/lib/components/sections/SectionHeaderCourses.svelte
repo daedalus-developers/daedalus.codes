581d054867c2d2f4a88773b06a7a4adbff9d0380
Denver Brian <89953243+DenverBrian@users.noreply.github.com>
2023-09-21 23:48:55 +0800

    #7 Courses Section Component  Fixes (#17)
    * #7 Courses Section Component  Fixes
    [x] create section header component for Courses.svelte
    
    * add cards and change font size of section header
    
    * add color in title and description
    
    * add  card border
    
    * add background svg and card description

---
+++ src/lib/components/sections/SectionHeaderCourses.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let title: string = 'Please Provide title',
+		description: string = 'Please provide Description';
+</script>
+
+<div class="mx-auto max-w-7xl px-6 lg:px-8">
+	<div class="mx-auto text-center flex justify-center dark:text-white">
+		<h2
+
+			class="text-sm font-bold basis-1/4 text-left tracking-tight
+			text-[#8D62EF]"
+		>
+			{title}
+		</h2>
+		<p class=" font-bold text-left tracking-tight text-gray-900 text-4xl dark:text-white">
+			{@html description}
+		</p>
+	</div>
+</div>
