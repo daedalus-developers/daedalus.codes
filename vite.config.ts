4f3bb07a581a97fd7383779f6d2dcf199fadbeda
Artifex <markasena@gmail.com>
2023-09-21 17:27:42 +0800

    🧹chore: nothing to say please review fast

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tsconfigPaths()],
 	test: {
+		globals: true,
+		environment: 'jsdom',
+		restoreMocks: true,
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		reporters: ['verbose', 'json'],
+		setupFiles: ['./src/test.config.ts'],
 		coverage: {
-			reporter: ['text', 'json-summary', 'json']
+			reporter: ['text', 'json-summary', 'json'],
+			exclude: ['./src/test.config.ts']
 		},
+		exclude: [...configDefaults.exclude, 'tests'],
 		outputFile: 'test-results/results.json'
 	}
 });
