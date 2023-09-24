6dffb40438d2ceddcccb65e20da1b63fa98efdd9
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-09-25 03:50:43 +0800

    fix responsive on small screen

--- src/lib/components/coursePath/StackChoice.svelte
+++ src/lib/components/coursePath/StackChoice.svelte
@@ -5,16 +5,16 @@
 <section class="mx-auto flex flex-col justify-center align-middle">
 	<div class="mx-auto flex justify-center text-center dark:text-white">
 		<h2
-			class="my-32 text-left text-7xl font-bold tracking-tight dark:text-white"
+			class="my-32 pl-4 text-left text-7xl font-bold tracking-tight dark:text-white"
 		>
 			Which <span class="text-[var(--color-accent)]">path</span> are you taking?
 		</h2>
 	</div>
-	<div class="mb-32 flex justify-center">
+	<div class="mb-32 flex flex-wrap justify-center">
 		<a
-			class="mx-3 w-72 cursor-pointer rounded-full
-             border bg-daedalusDark px-5 py-3 text-center text-white hover:bg-secondary hover:text-white dark:border-daedalusDark dark:bg-white
-             dark:text-black dark:hover:bg-secondary dark:hover:text-black"
+			class="mx-3 mb-2 w-72 cursor-pointer
+             rounded-full border bg-daedalusDark px-5 py-3 text-center text-white hover:bg-secondary hover:text-white dark:border-daedalusDark
+             dark:bg-white dark:text-black dark:hover:bg-secondary dark:hover:text-black"
 			href={frontEndUrl}
 		>
 			Front End
