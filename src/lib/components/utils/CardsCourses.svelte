723672f07628e5dc34dff9ad530a307393e6e8c6
devpapi0891 <rafael50891.dev@gmail.com>
2023-09-24 19:13:59 +0800

    adjust padding on small screen to fix over sized card

--- src/lib/components/utils/CardsCourses.svelte
+++ src/lib/components/utils/CardsCourses.svelte
@@ -5,7 +5,8 @@
 <div class="mx-auto flex max-w-7xl flex-col justify-center lg:flex-row">
 	{#each courses as course}
 		<div
-			class={`mx-auto mt-12 px-16 pt-6 basis-[calc(100%/${courses.length})] rounded-[100px] border-2 border-secondary text-center`}
+			data-course-item
+			class={`mx-auto mt-12 px-16 pt-12 basis-[calc(100%/${courses.length})] rounded-[100px] border-2 border-secondary text-center`}
 		>
 			<span
 				class="mx-auto text-4xl font-bold text-daedalusDark dark:text-white"
@@ -22,3 +23,13 @@
 		</div>
 	{/each}
 </div>
+
+<style>
+	@media only screen and (max-width: 600px) {
+		[data-course-item] {
+			font-size: 15px !important;
+			padding-left: 10px;
+			padding-right: 10px;
+		}
+	}
+</style>
