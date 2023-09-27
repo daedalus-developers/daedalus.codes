0d35d10f88ba1810cd51a0ad836535ef1498ba4c
Jake <110341554+os-hi@users.noreply.github.com>
2023-09-28 04:05:45 +0800

    🪄 style: Enhance Mission Component (#67)
    Fixes  #66

--- src/lib/components/Mission.svelte
+++ src/lib/components/Mission.svelte
@@ -3,7 +3,7 @@
 	let currentTab = 0;
 	const tabs: { title: string; description: string }[] = [
 		{ title: 'Mission', description: mission },
-		{ title: 'Vision', description: vision }
+		{ title: 'Vission', description: vision }
 	];
 	$: setCurrentTab = (id: number) => {
 		currentTab = id;
@@ -11,20 +11,21 @@
 </script>
 
 <div class="mx-auto flex max-w-7xl flex-col px-12 py-24">
-	<div class="flex-row pb-4">
+	<div class="flex-row pb-4 mb-4">
 		{#each tabs as tab, id}
 			<span
-				class="text-3xl font-semibold {currentTab === id
-					? 'rounded border-b-4 border-solid border-secondary text-secondary '
+				class="hover:border-b-secondary text-3xl font-semibold m-3 {currentTab === id
+					? 'rounded border-b-4 border-solid border-secondary text-secondary'
 					: 'text-black dark:text-white'}"
 				class:active={currentTab === id}
+				
 			>
 				<button on:click={() => setCurrentTab(id)}>{tab.title}</button>
 			</span>
 		{/each}
 	</div>
 	<div
-		class="rounded border-l-4 border-solid border-secondary p-4 font-serif text-2xl italic"
+		class="rounded border-l-4 border-solid border-secondary p-4 pl-10 font-serif text-2xl italic"
 	>
 		{tabs[currentTab].description}
 	</div>
