c22384cc39d55cd7a45ab0db5b15438bb56eb4c8
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-09-28 04:44:02 +0800

    #47 #48 Fix all navbar - related issues (#68)

--- src/lib/components/utils/ToggleTheme.svelte
+++ src/lib/components/utils/ToggleTheme.svelte
@@ -2,6 +2,7 @@
 	import { browser } from '$app/environment';
 	import Icon from '@iconify/svelte';
 	let darkMode = true;
+	export let mobile = false;
 	function handleSwitchDarkMode() {
 		darkMode = !darkMode;
 		localStorage.setItem('theme', darkMode ? 'dark' : 'light');
@@ -13,7 +14,8 @@
 	if (browser) {
 		if (
 			localStorage.theme === 'dark' ||
-			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+			(!('theme' in localStorage) &&
+				window.matchMedia('(prefers-color-scheme: dark)').matches)
 		) {
 			document.documentElement.classList.add('dark');
 			darkMode = true;
@@ -25,18 +27,45 @@
 </script>
 
 <!-- <label class="relative inline-flex items-center cursor-pointer"> -->
-<label class="cursor-pointer">
-	<input
-		type="checkbox"
-		value={darkMode}
-		id="theme-toggle"
-		on:click={handleSwitchDarkMode}
-		class="sr-only peer"
-	/>
-	<span class:hidden={!darkMode}>
-		<Icon icon="flat-color-icons:flash-on" class="text-2xl" />
-	</span>
-	<span class:hidden={darkMode}>
-		<Icon icon="flat-color-icons:flash-off" class="text-2xl" />
-	</span>
-</label>
+{#if mobile}
+	<label class="flex w-full cursor-pointer justify-center">
+		<input
+			type="checkbox"
+			value={darkMode}
+			id="theme-toggle"
+			on:click={handleSwitchDarkMode}
+			class="peer sr-only"
+		/>
+		<span
+			class="-mx-3 inline-flex items-center rounded-lg px-3 py-1 text-center text-base font-semibold leading-5 text-gray-900 hover:bg-gray-50 dark:text-white hover:dark:text-black"
+			class:hidden={!darkMode}
+		>
+			<Icon icon="flat-color-icons:flash-on" class="text-2xl " />
+
+			Turn off Dark mode
+		</span>
+		<span
+			class="-mx-3 inline-flex items-center rounded-lg px-3 py-1 text-center text-base font-semibold leading-5 text-white hover:bg-gray-50 hover:text-black dark:text-white hover:dark:text-black"
+			class:hidden={darkMode}
+		>
+			<Icon icon="flat-color-icons:flash-off" class="text-2xl" />
+			Turn on Dark mode
+		</span>
+	</label>
+{:else}
+	<label class="cursor-pointer">
+		<input
+			type="checkbox"
+			value={darkMode}
+			id="theme-toggle"
+			on:click={handleSwitchDarkMode}
+			class="peer sr-only"
+		/>
+		<span class:hidden={!darkMode}>
+			<Icon icon="flat-color-icons:flash-on" class="text-2xl" />
+		</span>
+		<span class:hidden={darkMode}>
+			<Icon icon="flat-color-icons:flash-off" class="text-2xl" />
+		</span>
+	</label>
+{/if}
--- src/routes/(landing)/Header.svelte
+++ src/routes/(landing)/Header.svelte
@@ -8,9 +8,9 @@
 
 <!-- NOTE: https://www.braydoncoyer.dev/blog/build-a-glassmorphic-navbar-with-tailwindcss-backdrop-filter-and-backdrop-blur -->
 <div
-	class="lg:sticky lg:top-0 lg:z-10 lg:bg-opacity-90 lg:pt-2 lg:backdrop-blur-md lg:backdrop-filter"
+	class="sticky left-0 top-0 z-10 backdrop-blur-md lg:sticky lg:bg-opacity-90"
 >
-	<header class="mx-auto max-w-7xl justify-center lg:pt-2">
+	<header class="mx-auto max-w-7xl justify-center">
 		<nav
 			class="mx-auto flex max-w-7xl items-center justify-between bg-gray-600 bg-opacity-5
          p-6 backdrop-filter lg:rounded lg:border lg:border-gray-800 lg:px-8"
@@ -19,8 +19,10 @@
 			<a href="/" class="-m-1.5 p-1.5">
 				<span class="sr-only">Daedalus</span>
 
-				<span class="h-8 w-auto text-2xl font-black text-black dark:text-white lg:block"
-					>Daedal<span class="font-black text-accent dark:text-accent">us</span></span
+				<span
+					class="h-8 w-auto text-2xl font-black text-black dark:text-white lg:block"
+					>Daedal<span class="font-black text-accent dark:text-accent">us</span
+					></span
 				>
 			</a>
 			<div class="flex lg:hidden">
@@ -52,21 +54,30 @@
 			</div>
 		</nav>
 		<!-- Mobile menu, show/hide based on menu open state. -->
-		<div class="lg:hidden" {hidden} role="dialog" aria-modal="true">
+		<div class=" lg:hidden" {hidden} role="dialog" aria-modal="true">
 			<!-- Background backdrop, show/hide based on slide-over state. -->
 			<div
-				class="z-15 fixed inset-0 bg-opacity-25 backdrop-blur-md backdrop-filter"
+				class="fixed inset-0 z-10 cursor-auto bg-opacity-25 backdrop-blur-[2px] backdrop-filter"
 				{hidden}
-			/>
+				on:click={() => (hidden = !hidden)}
+				role="button"
+				tabindex="0"
+				on:keydown={(event) => {
+					if (event.key === 'Enter' || event.key === ' ') {
+						hidden = !hidden;
+					}
+				}}
+			></div>
 			<div
-				class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+				data-navbar-container
+				class="navbarBackground fixed inset-y-0 right-0 z-10 w-full overflow-y-auto border border-gray-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
 			>
 				<div class="flex items-center justify-between">
 					<a href="/" class="-m-1.5 p-1.5">
 						<span class="sr-only">Daedalus</span>
 						<span
-							class="h-8 w-auto text-2xl font-black text-black dark:text-white"
-							>Daedal<span class="font-black text-accent dark:text-primary"
+							class="h-8 w-auto text-2xl font-black text-white dark:text-white"
+							>Daedal<span class="font-black text-accent dark:text-accent"
 								>us</span
 							></span
 						>
@@ -81,26 +92,32 @@
 					</button>
 				</div>
 				<div class="mt-6 flow-root">
-					<div class="-my-6 divide-y divide-gray-500/10">
+					<div class="divide-white-700 -my-6 divide-y">
 						<div class="space-y-2 py-6">
 							{#each links as link}
 								<a
 									href={link.link}
-									class="-mx-3 block rounded-lg px-3 py-1 text-center font-semibold leading-5 text-gray-900 hover:bg-gray-50 active:text-primary dark:text-white dark:hover:text-black"
-									class:active={$page.url.pathname === link.link}>{link.text}</a
-								>
+									class="-mx-3 block rounded-lg px-3 py-1 text-center font-semibold leading-5 text-white hover:bg-gray-50 hover:text-black active:text-primary dark:text-white hover:dark:text-black"
+									class:active={$page.url.pathname === link.link}
+									on:click={() => (hidden = !hidden)}
+									>{link.text}
+								</a>
 							{/each}
-							<ToggleTheme />
+							<div
+								class="  w-full rounded-lg px-3 py-1 hover:bg-gray-50 hover:text-black dark:text-white hover:dark:text-black"
+							>
+								<ToggleTheme mobile={true} />
+							</div>
 						</div>
 						<div class="py-6">
 							<a
 								href="/register"
-								class="-mx-3 block rounded-lg px-3 py-1 text-center text-base font-semibold leading-5 text-gray-900 hover:bg-gray-50 dark:text-white"
+								class="-mx-3 block rounded-lg px-3 py-1 text-center text-base font-semibold leading-5 text-white hover:bg-gray-50 hover:text-black dark:text-white hover:dark:text-black"
 								>Sign up</a
 							>
 							<a
 								href="/Login"
-								class="-mx-3 block rounded-lg px-3 py-1 text-center text-base font-semibold leading-5 text-gray-900 hover:bg-gray-50 dark:text-white"
+								class="-mx-3 block rounded-lg px-3 py-1 text-center text-base font-semibold leading-5 text-white hover:bg-gray-50 hover:text-black dark:text-white hover:dark:text-black"
 								>Login</a
 							>
 						</div>
@@ -112,5 +129,17 @@
 </div>
 
 <style>
-	
+	[data-navbar-container].navbarBackground::before {
+		content: ''; /* Required to create a pseudo-element */
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		z-index: -1;
+
+		background-color: var(--color-primary);
+		opacity: 0.5;
+		filter: blur(25px);
+	}
 </style>
