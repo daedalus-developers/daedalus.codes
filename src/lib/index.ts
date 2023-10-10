bc0bc1d15222c5aeff3de2f35bb05ba1e132babd
Artifex <markasena@gmail.com>
2023-10-11 06:43:09 +0800

    ✨feat: singleton db

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,9 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { PUBLIC_PB_URL } from '$env/static/public';
+import PocketBase from 'pocketbase';
+
+export function createClient() {
+	return new PocketBase(PUBLIC_PB_URL);
+}
+
+export const db = createClient();
