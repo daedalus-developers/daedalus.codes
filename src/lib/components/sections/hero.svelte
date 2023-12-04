51a82a401f5533c6fb536a39787550a96b3ac308
Jake <110341554+os-hi@users.noreply.github.com>
2023-12-04 22:55:25 +0800

    feat: issue#95 added homepage

---
+++ src/lib/components/sections/hero.svelte
@@ -0,0 +1,18 @@
+<div class="absolute w-64 h-56 lg:w-96 lg:h-80 rounded-full right-0 top-32 bg-purple-800 filter blur-[150px] z-0"></div>
+<div class="flex flex-col items-center my-10 lg:my-0 relative lg:px-28 lg:flex-row z-1">
+    <div class="w-full flex flex-wrap text-center gap-10 justify-center items-center lg:text-left lg:flex-col lg:flex-start  lg:gap-20 ">
+        <h1 class="text-5xl lg:text-7xl font-extrabold lg:tracking-wide leading-normal text-white">
+            Carry the world with our craftmanship
+        </h1>
+        <p class="text-white text-lg lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet at, 
+            quas officiis non ut quod sed soluta aut, repudiandae facilis quasi! 
+            Non magnam et quam quaerat nemo error officiis voluptatum porro sapiente 
+            corporis dolorum dicta deserunt recusandae, sit dolores aliquam, 
+            obcaecati ipsa repellat soluta optio fuga saepe eius eaque. Corporis.
+        </p>
+    </div>
+    <img src={"https://storage.daedalus.codes/hero.png"} alt="hero" class="w-72 lg:w-full  lg:h-full h-auto ">
+</div>
+<div class="absolute w-60 h-52 lg:w-96 lg:h-80 rounded-full left-0 bottom-32 bg-purple-800 filter blur-[150px] z-0"></div>
+
