2ac451dd9f9437c3be54d212a68ca2e1fc306cff
devpapi0891 <rafael50891.dev@gmail.com>
2023-12-05 03:05:22 +0800

    super change

--- src/lib/components/layouts/default-layout.svelte
+++ src/lib/components/layouts/default-layout.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
 	import { AppShell } from '@skeletonlabs/skeleton';
 	import { Footer, Header } from '../sections/';
+	
+	import type { ComponentEvents } from 'svelte';
+
+	function scrollHandler(event: ComponentEvents<AppShell>['scroll']) {
+		console.log(event.currentTarget.scrollTop);
+	}
+
 </script>
 
-<AppShell 
-	slotHeader="sticky top-0">
+<AppShell on:scroll={scrollHandler}>
 	<svelte:fragment slot="header">
-		<Header />
+		<div class="hidden md:block">
+			<Header />
+		</div>
 	</svelte:fragment>
 	<slot />
-	<svelte:fragment slot="footer">
+
+	<svelte:fragment slot="pageFooter">
 		<Footer />
+	</svelte:fragment>
+	<svelte:fragment slot="footer">
+		<div class="block md:hidden">
+			<Header />
+		</div>
 	</svelte:fragment>
 </AppShell>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,14 +6,30 @@ import '@fontsource-variable/inter';
 	import { initializeStores } from '@skeletonlabs/skeleton';
 	import { setupViewTransition } from 'sveltekit-view-transition';
   import { ModalRegistry, Drawers } from '@components';
+	import type { AfterNavigate } from '@sveltejs/kit';
+	import { afterNavigate } from '$app/navigation';
+	import { autoModeWatcher } from '@skeletonlabs/skeleton';
+
 
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 	initializeStores();
 	setupViewTransition();
 
+	afterNavigate((params: AfterNavigate) => {
+		const isNewPage = params.from?.url.pathname !== params.to?.url.pathname;
+		const elemPage = document.querySelector('#page');
+		if (isNewPage && elemPage !== null) {
+				elemPage.scrollTop = 0;
+		}
+	});
+
 </script>
+
+<svelte:head>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head>
 
 <Drawers />
 <Modal components={ModalRegistry} />
 <Toast position="tr" />
 <slot />
+
+
