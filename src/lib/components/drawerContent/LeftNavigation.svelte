e7f332e089e47ef5bfd0a40c84bd03db4feafd48
devpapi0891 <97945953+devpapi0891@users.noreply.github.com>
2023-12-04 22:59:04 +0800

    Devpapi0891/issue94 (#103)
    * install iconify as dev dependency
    
    * add assets for header
    
    * change h-screen to min-h-screen
    
    * move original theme.ts to theme-original.ts
    
    * add new export for components DIR utilities
    create LeftNavigation component
    add LeftNavigation component to exporter
    default-layout.svelte add props (slotHeader="sticky top-0")
    add conditional statement to drawers.svelte
    import LeftNavigation to drawers.svelte
    implement default-header.svelte design
    create component Container
    add Container component to barrel file exporter

---
+++ src/lib/components/drawerContent/LeftNavigation.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
+	import { page } from '$app/stores';
+  import { primaryLinks, secondaryLinks } from '@utils';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
+</script>
+
+<nav class="min-h-screenX group list-nav flex flex-col h-full">
+	<ul class="pb-20 pt-5 h-full overflow-auto">
+		{#each primaryLinks as { name, href, iconifyCode }}
+			<li
+				class={$page.url.pathname === href ? 'variant-ghost-primaryX variant-filled-primary' : ''}
+			>
+				<a {href} on:click={drawerClose}>
+					<Icon icon={iconifyCode} class="md:text-3xl" />
+					<span class="flex-auto">{name}</span>
+				</a>
+			</li>
+		{/each}
+    {#each secondaryLinks as { name, href, iconifyCode }}
+			<li
+				class={$page.url.pathname === href ? 'variant-ghost-primaryX variant-filled-primary' : ''}
+			>
+				<a {href} on:click={drawerClose}>
+					<Icon icon={iconifyCode} class="md:text-3xl" />
+					<span class="flex-auto">{name}</span>
+				</a>
+			</li>
+		{/each}
+	</ul>
+	<hr class="mt-auto">
+	<nav class="py-5 px-3">
+		<ul>
+			<li class="flex justify-center">
+				<a href="/"
+					on:click|preventDefault>
+					<LightSwitch class="rotate-90X" />
+					<!-- <span class="ms-2">Theme</span> -->
+				</a>
+			</li>
+			<li>
+				<a href="/login">
+					<Icon icon="material-symbols:login" class="text-2xl" />
+					<span class="ms-2">Login</span>
+				</a>
+			</li>
+			
+			
+		</ul>
+	</nav>
+</nav>
---
+++ src/lib/utils/index.ts
@@ -0,0 +1,45 @@
+export const primaryLinks = [
+  {
+    iconifyCode: 'ion:home-sharp',
+    name: 'Home',
+    href: '/'
+  },
+  {
+    iconifyCode: 'mdi:about-circle-outline',
+    name: 'About',
+    href: '/about'
+  },
+  {
+    iconifyCode: 'zondicons:portfolio',
+    name: 'Portfolio',
+    href: '/portfolio'
+  },
+  {
+    iconifyCode: 'fluent:people-team-20-filled',
+    name: 'Team',
+    href: '/team'
+  },
+  {
+    iconifyCode: 'clarity:event-solid',
+    name: 'Events',
+    href: '/events'
+  }
+]
+
+export const secondaryLinks = [
+  {
+    iconifyCode: 'clarity:event-solid',
+    name: 'Courses',
+    href: '/courses'
+  },
+  {
+    iconifyCode: 'ic:baseline-phone',
+    name: 'Contact',
+    href: '/contact'
+  }
+]
+
+export default {
+  primaryLinks,
+  secondaryLinks
+}
