467a9526384f15ad7a4ae0e6d8db62a8269f870b
Artifex <mac@mkra.dev>
2023-12-06 00:35:12 +0800

    fix: removed scroll watcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,9 @@
 	import { Modal, Toast, storePopup } from '@skeletonlabs/skeleton';
 	import { initializeStores } from '@skeletonlabs/skeleton';
 	import { setupViewTransition } from 'sveltekit-view-transition';
-
-  import { ModalRegistry, Drawers } from '@components';
+	import { ModalRegistry, Drawers } from '@components';
 	import type { AfterNavigate } from '@sveltejs/kit';
 	import { afterNavigate } from '$app/navigation';
-	import { autoModeWatcher } from '@skeletonlabs/skeleton';
 
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 	initializeStores();
@@ -19,16 +17,12 @@
 		const isNewPage = params.from?.url.pathname !== params.to?.url.pathname;
 		const elemPage = document.querySelector('#page');
 		if (isNewPage && elemPage !== null) {
-				elemPage.scrollTop = 0;
+			elemPage.scrollTop = 0;
 		}
 	});
 </script>
 
-<svelte:head>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head>
-
 <Drawers />
 <Modal components={ModalRegistry} />
-<Toast position="tr" />
+<Toast position="br" />
 <slot />
-
-
