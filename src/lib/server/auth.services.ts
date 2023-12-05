f43247949c3ba6b244cae70d95d9c2908e7006f3
Artifex <mac@mkra.dev>
2023-12-06 00:32:57 +0800

    feat: auth service wip

---
+++ src/lib/server/auth.services.ts
@@ -0,0 +1,59 @@
+import { type UserRole, Collections, userRole } from '@types';
+import { db } from '.';
+import type { ClientResponseError } from 'pocketbase';
+import { fail, redirect } from '@sveltejs/kit';
+
+export const isExisting = async (collection: Collections, field: string, value: string) =>
+	await db
+		.collection(collection)
+		.getFirstListItem(`${field}="${value}"`)
+		.then((result) => (result.id ? true : false))
+		.catch((error) => {
+			const err = error as ClientResponseError;
+			if (err.status === 404) return false;
+			return false;
+		});
+
+export const authenticateUser = (locals: App.Locals) => {
+	return locals.DB.authStore.isValid && locals.DB.authStore.model
+		? () => {
+				locals.user = locals.DB.authStore.model;
+				locals.user?.role === userRole.enum.user
+					? () => {
+							throw redirect(303, '/admin');
+					  }
+					: () => {
+							throw redirect(303, '/profile');
+					  };
+		  }
+		: () => {
+				throw fail(400, {});
+		  };
+};
+
+export const logout = (locals: App.Locals) => {
+	locals.DB.authStore.clear();
+	locals.user = undefined;
+
+	return;
+};
+
+export const authenticationGuard = (locals: App.Locals) => {
+	if (!locals.DB.authStore.isValid) throw redirect(303, '/login');
+	if (!locals.DB.authStore.token) throw redirect(303, '/login');
+	if (!locals.user?.id) throw redirect(303, '/login');
+
+	return;
+};
+
+export const authorizationGuard = (locals: App.Locals, requiredRole: UserRole) => {
+	if (!requiredRole !== locals.user?.role) throw redirect(307, '/unauthorized');
+
+	return;
+};
+
+export const authRefresh = async (locals: App.Locals) => {
+	await locals.DB.collection(Collections.Users).authRefresh();
+	authenticationGuard(locals);
+	return;
+};
