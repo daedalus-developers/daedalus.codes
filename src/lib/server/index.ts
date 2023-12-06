b123fdf9ab6bd75e4078b4e428157e8356216e36
Artifex <5866196+directormac@users.noreply.github.com>
2023-12-06 08:31:18 +0800

    revert: removed logging cloudflare does not support it (#111)
    Co-authored-by: Artifex <mac@mkra.dev>

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,15 +1,13 @@
 import { env as publicEnv } from '$env/dynamic/public';
 import { env } from '$env/dynamic/private';
 import PocketBase from 'pocketbase';
-import { logger } from './logger';
 
 export const createClient = async () => {
 	const pocketbase = new PocketBase(publicEnv.PUBLIC_PB_URL);
 	try {
-		logger.info(`Pocketbase host: ${publicEnv.PUBLIC_PB_URL}\n`);
 		await pocketbase.admins.authWithPassword(env.PB_ADMIN_EMAIL, env.PB_ADMIN_PASSWORD);
 	} catch (e) {
-		logger.error(e);
+		console.log(e);
 	}
 	return pocketbase;
 };
--- src/lib/server/logger.ts
+++
@@ -1,9 +0,0 @@
-import pino from 'pino';
-import pretty from 'pino-pretty';
-
-const pinoPretty = pretty({
-	colorize: true,
-	translateTime: 'yyyy-mm-dd HH:MM:ss.l'
-});
-
-export const logger = pino(pinoPretty);
