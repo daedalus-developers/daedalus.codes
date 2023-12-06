0319582dcef44f341d40a9a6d45e966bdaa3fb1c
Artifex <mac@mkra.dev>
2023-12-06 14:00:05 +0800

    feat: pages for /me

--- src/routes/(me)/me/+page.server.ts
+++ src/routes/(me)/me/+page.server.ts
@@ -1,8 +1,21 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { db } from '@server';
+import { Collections, userSchema, type User } from '@types';
+import { superValidate } from 'sveltekit-superforms/server';
 
-export const load: PageServerLoad = ({ locals }) => {
-	if (!locals.DB.authStore.isValid) throw redirect(302, '/login');
+export const load: PageServerLoad = async ({ locals }) => {
+	if (!locals.DB.authStore.isValid && !locals.user) throw redirect(302, '/login');
 
-	return {};
+	if (!locals.user) throw redirect(302, '/login');
+	const id: string = locals.user.id;
+
+	const user = await db.collection<User>(Collections.Users).getOne(id);
+
+	const form = await superValidate(user, userSchema);
+
+	return {
+		user,
+		form
+	};
 };
