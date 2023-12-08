9e13c89f85aff1ad7de84943fabb2087a668deea
devpapi0891 <rafael50891.dev@gmail.com>
2023-12-08 20:30:55 +0800

    logout button minor changes

--- src/lib/components/forms/logout-button.svelte
+++ src/lib/components/forms/logout-button.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <form method="POST" action="/api/actions/auth?/logout">
-	<button type="submit" class="flex">
-		<Icon icon="mdi:logout" height="24" />
+	<button type="submit" class="flex items-center gap-1 p-2 hover:variant-ghost w-full">
+		<Icon icon="mdi:logout" height="18" />
 		Logout
 	</button>
 </form>
