46116920c11f61cfe27f15e22df7b95c54bc568f
Frostzeichen <cruztjomar@gmail.com>
2023-12-09 03:33:30 +0800

    Feature: custom email validation message

--- src/lib/components/sections/FooterSubscribe.svelte
+++ src/lib/components/sections/FooterSubscribe.svelte
@@ -3,7 +3,10 @@
         <p class="font-bold">Subscribe to our newsletter</p>
         <p>Stay ahead in tech! Subscribe now for exclusive updates on mentorship, courses, and hackathons. Join our dynamic community and access valuable insights straight to your inbox. Don't miss out – subscribe today!</p>
         <form>
-            <input type="email" placeholder="Type your email...">
+            <input type="email" placeholder="Type your email..." name="email" value="asd@asd.com">
+            <div class="error">
+                Please enter a valid email
+            </div>
             <input type="submit" value="Subscribe">
         </form>
     </div>
@@ -11,3 +14,15 @@
         <p>Follow us</p>
     </div>
 </div>
+
+<style>
+    .error {
+        display: none;
+    }
+
+    input:invalid + .error {
+        display: initial;
+        color: red;
+        font-style: italic;
+    }
+</style>
