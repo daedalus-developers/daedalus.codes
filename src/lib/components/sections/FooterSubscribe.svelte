a14cc65fea41e602bf6ab00514379240898e9edf
Frostzeichen <cruztjomar@gmail.com>
2023-12-09 03:37:58 +0800

    Feat: add div wrapper to email bar and validation #100

--- src/lib/components/sections/FooterSubscribe.svelte
+++ src/lib/components/sections/FooterSubscribe.svelte
@@ -3,9 +3,11 @@
         <p class="font-bold">Subscribe to our newsletter</p>
         <p>Stay ahead in tech! Subscribe now for exclusive updates on mentorship, courses, and hackathons. Join our dynamic community and access valuable insights straight to your inbox. Don't miss out – subscribe today!</p>
         <form>
-            <input type="email" placeholder="Type your email..." name="email">
-            <div class="error">
-                Please enter a valid email
+            <div>
+                <input type="email" placeholder="Type your email..." name="email">
+                <div class="error">
+                    Please enter a valid email
+                </div>
             </div>
             <input type="submit" value="Subscribe">
         </form>
