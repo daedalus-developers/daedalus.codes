860f6abefffbe708dde2855ae3e59d41393d995e
Frostzeichen <cruztjomar@gmail.com>
2023-12-10 04:10:18 +0800

    Feat: add light and dark, web and mobile configurations

--- src/lib/components/sections/FooterSubscribe.svelte
+++ src/lib/components/sections/FooterSubscribe.svelte
@@ -7,14 +7,25 @@
         invalidEmail = !regexEmail.test(email);
         if (email.length < 3) invalidEmail = false;
     }
+
+    export let isMobile:boolean;
+    export let isDark:boolean;
+    
+    let backgroundColor:string;
+    let fontColor:string;
+    let warningColor: string;
+    let darkLightModes = [
+        {iisDark: true, backgroundColor: "#1A2024", fontColor: "#FFFFFF", warningColor: "#FFFFFF"},
+        {iisDark: false, backgroundColor: "#F6F8FB", fontColor: "#000000", warningColor: "#FF0909"},
+    ]
 </script>
 
-<div class="bg-[#F6F8FB]">
+<div class="bg-[{isDark === darkLightModes[0].iisDark ? darkLightModes[0].backgroundColor : darkLightModes[1].backgroundColor}]">
     <div class="flex flex-row flex-wrap gap-20 ml-24 mr-24 mt-12 mb-12">
         <div class="flex flex-1 flex-col gap-10">
-            <p class="font-semibold text-[##000000] text-2xl">Subscribe to our newsletter</p>
-            <p>Stay ahead in tech! Subscribe now for exclusive updates on mentorship, courses, and hackathons. Join our dynamic community and access valuable insights straight to your inbox. Don't miss out – subscribe today!</p>
-            <form class="flex flex-col" on:submit|preventDefault={handleSubmit}>
+            <p class="font-semibold text-[{isDark === darkLightModes[0].iisDark ? darkLightModes[0].fontColor : darkLightModes[1].fontColor}] text-2xl">Subscribe to our newsletter</p>
+            <p class="text-[{isDark === darkLightModes[0].iisDark ? darkLightModes[0].fontColor : darkLightModes[1].fontColor}] text-base">Stay ahead in tech! Subscribe now for exclusive updates on mentorship, courses, and hackathons. Join our dynamic community and access valuable insights straight to your inbox. Don't miss out – subscribe today!</p>
+            <form class="flex flex-col" on:submit|preventDefault>
                 <div class="flex flex-row gap-2">
                     <input 
                         type="email"
@@ -27,10 +38,12 @@
                     <input type="submit" value="Subscribe" class="btn bg-[#7A52B3] rounded text-[#FFFFFF] text-base font-semibold pl-10 pr-10">
                 </div>
                 {#if invalidEmail}
-                    <p class="text-[#FF0909] text-xs italic">Please enter a valid email</p>
+                    <p class="text-[{isDark === darkLightModes[0].iisDark ? darkLightModes[0].warningColor : darkLightModes[1].warningColor}] text-xs italic">Please enter a valid email</p>
                 {/if}
             </form>
         </div>
-        <div class="flex h-80 w-80 bg-orange-400" />
+        {#if !isMobile}
+            <div class="flex h-80 w-80 bg-orange-400" />
+        {/if}
     </div>
 </div>
--- src/lib/components/sections/default-footer.svelte
+++ src/lib/components/sections/default-footer.svelte
@@ -5,12 +5,15 @@
 	import FooterDaedalusEmail from './FooterDaedalusEmail.svelte';
 	import FooterDaedalusAddress from './FooterDaedalusAddress.svelte';
 	import FooterLogo from './FooterLogo.svelte';
+
+	export let isMobile = false;
+	export let isDark = false;
 </script>
 
 <AppShell>
 	<svelte:fragment slot="pageFooter">
 		<div class="flex flex-col">
-			<FooterSubscribe />
+			<FooterSubscribe isMobile={isMobile} isDark={isDark} />
 			<div class="flex flex-row gap-64 font-sans bg-[#101010] text-[#FFFFFF] pl-[10%] pt-[5%] pb-[10%] pr-[10%]">
 				<div class="flex gap-8 flex-col">
 					<FooterLogo />
@@ -22,5 +25,19 @@
 				</div>
 			</div>
 		</div>
+	</svelte:fragment>
+	<svelte:fragment slot="footer">
+		{#if isMobile}
+			<div class="flex flex-row bg-[#101010] text-[#FFFFFF] pb-[5%] pr-[10%] pl-[10%]">
+				<div class="flex flex-1">
+					<p>&copy {new Date().getFullYear()} All Rights Reserved.</p>
+				</div>
+				<div class="flex flex-1">
+					<a href="/privacy-policy">Privacy Policy</a>
+					<a href="/terms-of-service">Terms of Service</a>
+					<a href="/cookies-policy">Cookies Policy</a>
+				</div>
+			</div>
+		{/if}
 	</svelte:fragment>
 </AppShell>
