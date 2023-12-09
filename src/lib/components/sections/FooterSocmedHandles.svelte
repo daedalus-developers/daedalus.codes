3b62337de712fc2fc753ca96afa2f21723150278
Frostzeichen <cruztjomar@gmail.com>
2023-12-09 17:35:02 +0800

    Feat: add borders for socmed handle placeholders #100

--- src/lib/components/sections/FooterSocmedHandles.svelte
+++ src/lib/components/sections/FooterSocmedHandles.svelte
@@ -3,15 +3,15 @@
 </div>
 <div>
     <a href="twitter.com">
-        <div class="h-[66px] w-[65px] bg-orange-300 border-green-50" />
+        <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
     </a>
     <a href="facebook.com">
-        <div class="h-[66px] w-[65px] bg-orange-300" />
+        <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
     </a>
     <a href="linkedin.com">
-        <div class="h-[66px] w-[65px] bg-orange-300" />
+        <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
     </a>
     <a href="tiktok.com">
-        <div class="h-[66px] w-[65px] bg-orange-300" />
+        <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
     </a>
 </div>
