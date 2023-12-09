e5c327922dac44d5e2e22f5d07e2fdc9fb0a54a2
Artifex <mac@mkra.dev>
2023-12-09 12:28:28 +0800

    feat: new form from page load

--- src/routes/(me)/me/+page.server.ts
+++ src/routes/(me)/me/+page.server.ts
@@ -1,12 +1,16 @@
+import { db } from '@server';
 import type { PageServerLoad } from './$types';
-import { userDetailsFormSchema, userFormSchema } from '@types';
+import { Collections, userDetailsFormSchema, userFormSchema } from '@types';
 import { superValidate } from 'sveltekit-superforms/server';
 
 export const load: PageServerLoad = async ({ parent }) => {
 	const { user, avatar } = await parent();
+	const details = await db
+		.collection(Collections.UsersDetails)
+		.getFirstListItem(`user="${user.id}"`);
 
 	const userForm = await superValidate(user, userFormSchema);
-	const userDetailsForm = await superValidate(user, userDetailsFormSchema);
+	const userDetailsForm = await superValidate(details, userDetailsFormSchema);
 
 	return {
 		user,
