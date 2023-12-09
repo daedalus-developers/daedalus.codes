8ddec6eb32d6289b89dde58231d076ec12723253
Frostzeichen <cruztjomar@gmail.com>
2023-12-09 17:31:03 +0800

    Feat: add placeholders for missing logos

--- src/lib/components/sections/FooterSocmedHandles.svelte
+++ src/lib/components/sections/FooterSocmedHandles.svelte
@@ -1,3 +1,17 @@
 <div id="followUs">
     <p>Follow us</p>
 </div>
+<div>
+    <a href="twitter.com">
+        <div class="h-[66px] w-[65px] bg-orange-300 border-green-50" />
+    </a>
+    <a href="facebook.com">
+        <div class="h-[66px] w-[65px] bg-orange-300" />
+    </a>
+    <a href="linkedin.com">
+        <div class="h-[66px] w-[65px] bg-orange-300" />
+    </a>
+    <a href="tiktok.com">
+        <div class="h-[66px] w-[65px] bg-orange-300" />
+    </a>
+</div>
