4ce5067399eb330b0cd18b4589f861e5ede7ca43
Frostzeichen <cruztjomar@gmail.com>
2023-12-09 17:36:11 +0800

    Feat: add folluw us and scomed handle wrapper #100

--- src/lib/components/sections/FooterSocmedHandles.svelte
+++ src/lib/components/sections/FooterSocmedHandles.svelte
@@ -1,17 +1,19 @@
-<div id="followUs">
-    <p>Follow us</p>
-</div>
 <div>
-    <a href="twitter.com">
-        <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
-    </a>
-    <a href="facebook.com">
-        <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
-    </a>
-    <a href="linkedin.com">
-        <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
-    </a>
-    <a href="tiktok.com">
-        <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
-    </a>
+    <div id="followUs">
+        <p>Follow us</p>
+    </div>
+    <div>
+        <a href="twitter.com">
+            <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
+        </a>
+        <a href="facebook.com">
+            <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
+        </a>
+        <a href="linkedin.com">
+            <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
+        </a>
+        <a href="tiktok.com">
+            <div class="h-[66px] w-[65px] bg-orange-300 border-2" />
+        </a>
+    </div>
 </div>
