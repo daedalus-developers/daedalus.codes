b1b66ab9b17c3546fedfc00fde4b4fb0f44dc262
Frostzeichen <68489239+Frostzeichen@users.noreply.github.com>
2023-12-10 05:06:02 +0800

    feat: Footer #100 (#102)
    * Feature: add unstyled contents to footer
    
    * Refactor: icon styling from css to tailwind #100
    
    * Refactor: change submit button styling from css to tailwind #100
    
    * Feat: set lead divs to vertical #100
    
    * fix: swap positions between "sign up to newsletter" and "get in touch"
    
    * Feat: set trail divs to vertical #100
    
    * Refactor: remove unused divs and ids
    
    * Feat: set typography to font-sans #100
    
    * Feat: set "sign up to newsletter" form width, height, and subscribe button bg color
    
    * Refactor: change AppBar to AppShell and grids to flexbox. Retained previous layout. #100
    
    * Fix: add flex wrap to form
    
    * Fix: prevent border from going past the subscribe button
    
    * Revert: remove flex wrapping
    
    * Feat: set padding #100
    
    * Feat: set padding for bottom parts and remove unnecessary margin #100
    
    * Fix: adjust top padding
    
    * Refactor: move subscribe and logo to own component and remove styling #100
    
    * Feature: custom email validation message
    
    * Feat: remove dev placeholder value
    
    * Feat: add div wrapper to email bar and validation #100
    
    * Refactor: move socmed handles to own component #100
    
    * Feat: add placeholders for missing logos
    
    * Feat: add borders for socmed handle placeholders #100
    
    * Feat: add folluw us and scomed handle wrapper #100
    
    * Feat: set flexbox for scomed handles #100
    
    * Feat: set flex as column for scomed handle and follow us component #100
    
    * Refactor: move daedalus email to own component #100
    
    * Refactor: move Daedalus physical address to own component #100
    
    * Refactor: remove excessive divs while removing styling #100
    
    * Feat: add flex col wrappers #100
    
    * Feat: add flex col wrapper #100
    
    * Feat: add flexbox wrapper for subscribe and the rest of footer #100
    
    * Feat: change validation method for email to use if block instead of css
    
    * Feat: set flexbox for email, subscribe, and error message
    
    * Feat: set borders, border colors, submit button background, and font color and weight #100
    
    * Feat: set subscribe email font color and weight #100
    
    * Feat: set margin for FooterSubscribe component #100
    
    * Feat: add image placeholder #100
    
    * Feat: set flexbox for wrapper #100
    
    * Feat: set font size, color, and weight to "Subscribe to our newsletter" #100
    
    * Feat: set footer bg for subscribe to newsletter #100
    
    * Feat: set margins for subscribe to newsletter #100
    
    * Feat: set font sizes for email, subscribe, and error message
    
    * Feat: set paddings for email and subscribe button, flex wrap for subcribe to newsletter contents #100
    
    * Feat: set email bar witdh #100
    
    * Fix: Add missing footer placeholder
    
    * Feat: add spacing and font sizes #100
    
    * Feat: set top margin #100
    
    * Feat: update top margin
    
    * Feat: add font size and spacing #100
    
    * Feat: set spacing and font size #100
    
    * Feat: remove unused footer components #100
    
    * Feat: init mobile footer #100
    
    * Refactor: remove unused function
    
    * Revert: remove unused file #100
    
    * Feat: add light and dark, web and mobile configurations
    
    * Feature: add circle designs using css #100
    
    * Feat: adjust margins and padding for bottom part of footer #100
    
    * Feat: add flex wrap #100
    
    * Feat: add Daeddalus logo #100

--- src/lib/components/sections/default-footer.svelte
+++ src/lib/components/sections/default-footer.svelte
@@ -1,9 +1,68 @@
 <script lang="ts">
-	import { AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import FooterSubscribe from './FooterSubscribe.svelte';
+	import FooterSocmedHandles from './FooterSocmedHandles.svelte';
+	import FooterDaedalusEmail from './FooterDaedalusEmail.svelte';
+	import FooterDaedalusAddress from './FooterDaedalusAddress.svelte';
+	import FooterLogo from './FooterLogo.svelte';
+
+	export let isMobile = false;
+	export let isDark = false;
 </script>
 
-<AppBar>
-	<svelte:fragment slot="lead">(icon)</svelte:fragment>
-	<svelte:fragment slot="trail">(actions)</svelte:fragment>
-	<svelte:fragment slot="headline">(headline)</svelte:fragment>
-</AppBar>
+<AppShell>
+	<svelte:fragment slot="pageFooter">
+		<div class="flex flex-col">
+			<FooterSubscribe isMobile={isMobile} isDark={isDark} />
+			<div class="flex flex-row flex-wrap gap-64 font-sans bg-[#101010] text-[#FFFFFF] pl-[18%] pt-[5%] pb-[10%] pr-[10%] relative">
+				<div class="flex gap-8 flex-col">
+					<FooterLogo />
+					<FooterDaedalusEmail />
+					<FooterSocmedHandles />
+				</div>
+				<div class="flex flex-col">
+					<FooterDaedalusAddress />
+				</div>
+				<div id="leftCircle" />
+				<div id="rightCircle" />
+			</div>
+		</div>
+	</svelte:fragment>
+	<svelte:fragment slot="footer">
+		{#if isMobile}
+			<div class="flex flex-row bg-[#101010] text-[#FFFFFF] pb-[5%] pr-[10%] pl-[10%]">
+				<div class="flex flex-1">
+					<p>&copy {new Date().getFullYear()} All Rights Reserved.</p>
+				</div>
+				<div class="flex flex-1">
+					<a href="/privacy-policy">Privacy Policy</a>
+					<a href="/terms-of-service">Terms of Service</a>
+					<a href="/cookies-policy">Cookies Policy</a>
+				</div>
+			</div>
+		{/if}
+	</svelte:fragment>
+</AppShell>
+
+<style>
+	#leftCircle {
+		position: absolute;
+		left: -15%;
+		bottom: 25%;
+		width: 18%;
+		height: 56%;
+		background: #7952B3;
+		border-radius: 50%;
+		filter: blur(300.5px);
+	}
+
+	#rightCircle {
+		position: absolute;
+		right: 0px;
+		width: 100px;
+		height: 100px;
+		background: #00B6BF;
+		border-radius: 50%;
+		filter: blur(300.5px);
+	}
+</style>
