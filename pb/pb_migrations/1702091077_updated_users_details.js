154c68e4cce98bd430e9eaedd6ae0dda382d9327
Artifex <5866196+directormac@users.noreply.github.com>
2023-12-09 12:31:01 +0800

    feat: userdetails form (#138)
    * refactor: rename form to specific form
    
    * refactor: rename action to details
    
    * fix: update url data type to string
    
    * feat: new form from page load
    
    * feat: add FAILED message
    
    * fix: misleading form responses
    
    * feat: userdeatils-form and types aligned types from migration
    
    * feat: link input
    
    ---------
    
    Co-authored-by: Artifex <mac@mkra.dev>

---
+++ pb/pb_migrations/1702091077_updated_users_details.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("ke436i7mqpn0g09")
+
+  collection.listRule = "@request.auth.id != user.id"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("ke436i7mqpn0g09")
+
+  collection.listRule = null
+
+  return dao.saveCollection(collection)
+})
--- src/routes/(me)/me/+page.server.ts
+++ src/routes/(me)/me/+page.server.ts
@@ -1,15 +1,21 @@
+import { db } from '@server';
 import type { PageServerLoad } from './$types';
-import { userFormSchema } from '@types';
+import { Collections, userDetailsFormSchema, userFormSchema } from '@types';
 import { superValidate } from 'sveltekit-superforms/server';
 
 export const load: PageServerLoad = async ({ parent }) => {
 	const { user, avatar } = await parent();
+	const details = await db
+		.collection(Collections.UsersDetails)
+		.getFirstListItem(`user="${user.id}"`);
 
-	const form = await superValidate(user, userFormSchema);
+	const userForm = await superValidate(user, userFormSchema);
+	const userDetailsForm = await superValidate(details, userDetailsFormSchema);
 
 	return {
 		user,
 		avatar,
-		form
+		userForm,
+		userDetailsForm
 	};
 };
