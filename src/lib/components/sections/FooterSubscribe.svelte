3707cb1209b74f26b4746517e3da825fa04a3450
Frostzeichen <cruztjomar@gmail.com>
2023-12-10 01:21:31 +0800

    Feat: set margin for FooterSubscribe component #100

--- src/lib/components/sections/FooterSubscribe.svelte
+++ src/lib/components/sections/FooterSubscribe.svelte
@@ -13,7 +13,7 @@
     }
 </script>
 
-<div>
+<div class="m-11">
     <div>
         <p class="font-bold">Subscribe to our newsletter</p>
         <p>Stay ahead in tech! Subscribe now for exclusive updates on mentorship, courses, and hackathons. Join our dynamic community and access valuable insights straight to your inbox. Don't miss out – subscribe today!</p>
