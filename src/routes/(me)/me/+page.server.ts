6dc670ff21b67e8358be2e5d60fe85873804a001
Artifex <mac@mkra.dev>
2023-12-09 08:59:27 +0800

    fix: update file link generator from pocketbase

--- src/routes/(me)/me/+page.server.ts
+++ src/routes/(me)/me/+page.server.ts
@@ -1,21 +1,15 @@
-import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { db } from '@server';
-import { Collections, type User, userFormSchema } from '@types';
+import { userFormSchema } from '@types';
 import { superValidate } from 'sveltekit-superforms/server';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	if (!locals.DB.authStore.isValid && !locals.user) throw redirect(302, '/login');
-
-	if (!locals.user) throw redirect(302, '/login');
-	const id: string = locals.user.id;
-
-	const user = await db.collection(Collections.Users).getOne<User>(id);
+export const load: PageServerLoad = async ({ parent }) => {
+	const { user, avatar } = await parent();
 
 	const form = await superValidate(user, userFormSchema);
 
 	return {
 		user,
+		avatar,
 		form
 	};
 };
