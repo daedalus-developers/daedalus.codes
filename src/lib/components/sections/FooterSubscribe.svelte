8153b9d50e04a09dfc04e2fce53edff3d5c2607e
devpapi0891 <97945953+devpapi0891@users.noreply.github.com>
2023-12-11 01:11:50 +0800

    Devpapi0891/issue142 (#144)
    * move unused components to unused-components DIR
    
    * add Events component for homepage
    
    * create FooterComponent Desktop view
    create FooterComponent Mobile view
    re-layout FooterSubscribe component
    
    * import Events component in home page.
    remove unused components from dafault-footer.svelte
    
    * Correct wrong title for home page Events component

--- src/lib/components/sections/FooterSubscribe.svelte
+++ src/lib/components/sections/FooterSubscribe.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { Container } from '@components/utilities/';
+	import Icon from '@iconify/svelte';
+
 	let invalidEmail = false;
 	const regexEmail = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$', 'g');
 
@@ -9,19 +12,48 @@
 		if (email.length < 3) invalidEmail = false;
 	};
 
-	export let isMobile: boolean;
 	export let isDark: boolean;
-
-	// let backgroundColor: string;
-	// let fontColor: string;
-	// let warningColor: string;
-	let darkLightModes = [
-		{ iisDark: true, backgroundColor: '#1A2024', fontColor: '#FFFFFF', warningColor: '#FFFFFF' },
-		{ iisDark: false, backgroundColor: '#F6F8FB', fontColor: '#000000', warningColor: '#FF0909' }
-	];
 </script>
 
-<div
+
+<section class="py-32 {!isDark? 'bg-surface-100-800-token' : ''}">
+	<Container addClass="flex flex-col md:flex-row items-center">
+		<div class="flex flex-col gap-8 md:gap-10 text-center md:text-left">
+			<h3 class="text-3xl md:text-4xl font-bold 
+			dark:bg-gradient-to-r dark:from-primary-600 dark:to-secondary-600 dark:bg-clip-text dark:text-transparent">Subscribe to our newsletter</h3>
+			<p>Stay ahead in tech! Subscribe now for exclusive updates on mentorship, courses, and hackathons. Join our dynamic community and access valuable insights straight to your inbox. Don't miss out – subscribe today!</p>
+			<form class="flex flex-col" on:submit|preventDefault>
+				<div class="flex flex-row gap-2">
+					<input
+						type="email"
+						placeholder="Type your email..."
+						name="email"
+						on:input={emailValidation}
+						bind:value={email}
+						class="border-spacing-[1.2px] border-[#7A52B3] rounded text-[#232323] text-base font-normal px-5 w-10/12"
+					/>
+					<input
+						type="submit"
+						value="Subscribe"
+						class="btn variant-filled-secondary rounded"
+					/>
+				</div>
+				{#if invalidEmail}
+					<p class="text-sm italic text-error-500 text-left">Please enter a valid email</p>
+				{/if}
+			</form>
+		</div>
+		<div class="calendar aspect-square w-96 hidden md:flex justify-center items-center p-10">
+			<span class="text-[200px] text-primary-600">
+				<Icon icon="clarity:calendar-solid" class=" skew-y-6 transform-gpu" />
+			</span>
+		</div>
+	</Container>
+</section>
+
+<!-- ----------------------------------- -->
+
+<!-- <div
 	class="bg-[{isDark === darkLightModes[0].iisDark
 		? darkLightModes[0].backgroundColor
 		: darkLightModes[1].backgroundColor}]"
@@ -75,4 +107,4 @@
 			<div class="flex h-80 w-80 bg-orange-400" />
 		{/if}
 	</div>
-</div>
+</div> -->
