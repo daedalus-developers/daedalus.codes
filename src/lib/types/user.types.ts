5dc3303574e9ffe35122d024802c159d39ed19e1
benok <jovenvictorlogo@gmail.com>
2023-12-12 10:24:40 +0800

    feat: Add type for Paginated Team

--- src/lib/types/user.types.ts
+++ src/lib/types/user.types.ts
@@ -112,3 +112,11 @@ export const avatarSchema = object({
 export type Avatar = zInfer<typeof avatarSchema>;
 
 export type Team = User & Partial<{ userDetails: UserDetails }>;
+
+export type PaginatedTeam = {
+	page?: number;
+	perPage?: number;
+	totalItems?: number;
+	totalPages?: number;
+	items?: Team[];
+};
