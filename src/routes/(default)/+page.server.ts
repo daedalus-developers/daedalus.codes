b725db22ce3732b265c68906971508923754bbeb
benok <jovenvictorlogo@gmail.com>
2023-12-12 10:24:17 +0800

    refactor: Refactor fetching of data

--- src/routes/(default)/+page.server.ts
+++ src/routes/(default)/+page.server.ts
@@ -1,23 +1,14 @@
-import { db } from '@server';
 import type { PageServerLoad } from './$types';
-import type { User, Project, UserDetails, Team } from '@types';
+import type { Project, PaginatedTeam } from '@types';
+import { fetchTeam, getAllProjects } from '@server';
 
 export const load: PageServerLoad = async () => {
 	let projects: Project[] = [];
-	let team: Team[] = [];
+	let team: PaginatedTeam = {};
 
 	// PROJECTS
 	try {
-		const projectList = (await db.collection('projects').getFullList({
-			sort: 'created'
-		})) as Project[];
-
-		projects = projectList.map((project) => {
-			return {
-				...project,
-				preview: db.files.getUrl(project, project.preview)
-			};
-		});
+		projects = await getAllProjects();
 	} catch (error) {
 		if (error instanceof Error) {
 			console.error(error.message);
@@ -26,22 +17,9 @@ export const load: PageServerLoad = async () => {
 
 	// TEAM & USER DETAILS
 	try {
-		const users = (await db.collection('users').getFullList({
-			sort: 'created',
-			filter: 'role="team"'
-		})) as User[];
-
-		const userDetails = (await db.collection('users_details').getFullList({
-			sort: 'created'
-		})) as UserDetails[];
-
-		team = users.map((user) => {
-			return {
-				...user,
-				avatar: db.files.getUrl(user, user.avatar),
-				userDetails: userDetails.find((detail) => detail.user === user.id)
-			};
-		});
+		const pageNumber = 1;
+		const perPage = 3;
+		team = await fetchTeam(pageNumber, perPage);
 	} catch (error) {
 		if (error instanceof Error) {
 			console.error(error.message);
