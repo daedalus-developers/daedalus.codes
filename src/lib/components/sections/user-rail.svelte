a16490e2dcc6d2c8fd01ad08299469f0617f3cfe
Artifex <5866196+directormac@users.noreply.github.com>
2023-12-14 23:32:57 +0800

    refactor: mobile responsive apprail (#153)
    Co-authored-by: Artifex <mac@mkra.dev>

--- src/lib/components/sections/user-rail.svelte
+++ src/lib/components/sections/user-rail.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import LogoutButton from '@components/forms/logout-button.svelte';
 	import Icon from '@iconify/svelte';
 	import { AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
 </script>
@@ -7,33 +6,37 @@
 <AppRail>
 	<svelte:fragment slot="lead">
 		<AppRailAnchor href="/me">
-			<div class="flex items-center">
+			<div class="flex flex-col items-center text-lg">
 				<Icon icon="mdi:user" width="24" height="24" />
 				Me
 			</div>
 		</AppRailAnchor>
 	</svelte:fragment>
 	<AppRailAnchor href="/me/details">
-		<div class="flex items-center">
+		<div class="flex flex-col items-center text-lg">
 			<Icon icon="mdi:details" width="24" height="24" />
 			Details
 		</div>
 	</AppRailAnchor>
 	<AppRailAnchor href="/me/account">
-		<div class="flex items-center">
+		<div class="flex flex-col items-center text-lg">
 			<Icon icon="mdi:form" width="24" height="24" />
 			Account
 		</div>
 	</AppRailAnchor>
 	<AppRailAnchor href="/me/settings">
-		<div class="flex items-center">
+		<div class="flex flex-col items-center text-lg">
 			<Icon icon="mdi:gear" width="24" height="24" />
 			Settings
 		</div>
 	</AppRailAnchor>
 	<svelte:fragment slot="trail">
-		<div class="py-4">
-			<LogoutButton />
+		<div class="py-4 text-sm">
+			<form method="POST" action="/api/actions/auth?/logout">
+				<button type="submit" class="btn">
+					<Icon icon="mdi:logout" />
+				</button>
+			</form>
 		</div>
 	</svelte:fragment>
 </AppRail>
