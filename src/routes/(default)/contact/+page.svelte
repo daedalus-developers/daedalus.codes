19a44b6497973f5d6a2b871aa962fcf98c302dfe
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-12-14 23:42:22 +0800

    #125 feat: Add ContactForm component to contact page

--- src/routes/(default)/contact/+page.svelte
+++ src/routes/(default)/contact/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+	import ContactForm from '@components/forms/contact-form.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	console.log('🚀 ~ file: +page.svelte:5 ~ data:', data);
 </script>
 
 <!-- TODO: update this placeholder UI from TailwindUI  -->
@@ -120,82 +120,6 @@
 				</dl>
 			</div>
 		</div>
-		<form action="#" method="POST" class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
-			<div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
-				<div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-					<div>
-						<label for="first-name" class="block text-sm font-semibold leading-6 text-white"
-							>First name</label
-						>
-						<div class="mt-2.5">
-							<input
-								type="text"
-								name="first-name"
-								id="first-name"
-								autocomplete="given-name"
-								class="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-							/>
-						</div>
-					</div>
-					<div>
-						<label for="last-name" class="block text-sm font-semibold leading-6 text-white"
-							>Last name</label
-						>
-						<div class="mt-2.5">
-							<input
-								type="text"
-								name="last-name"
-								id="last-name"
-								autocomplete="family-name"
-								class="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-							/>
-						</div>
-					</div>
-					<div class="sm:col-span-2">
-						<label for="email" class="block text-sm font-semibold leading-6 text-white">Email</label
-						>
-						<div class="mt-2.5">
-							<input
-								type="email"
-								name="email"
-								id="email"
-								autocomplete="email"
-								class="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-							/>
-						</div>
-					</div>
-					<div class="sm:col-span-2">
-						<label for="phone-number" class="block text-sm font-semibold leading-6 text-white"
-							>Phone number</label
-						>
-						<div class="mt-2.5">
-							<input
-								type="tel"
-								name="phone-number"
-								id="phone-number"
-								autocomplete="tel"
-								class="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-							/>
-						</div>
-					</div>
-					<div class="sm:col-span-2">
-						<label for="message" class="block text-sm font-semibold leading-6 text-white"
-							>Message</label
-						>
-						<div class="mt-2.5">
-							<textarea
-								name="message"
-								id="message"
-								rows="4"
-								class="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-							></textarea>
-						</div>
-					</div>
-				</div>
-				<div class="mt-8 flex justify-end">
-					<button type="submit" class="btn variant-filled-primary">Send message</button>
-				</div>
-			</div>
-		</form>
+		<ContactForm />
 	</div>
 </div>
