a50cfde313c66da8c655cce8ce8c3dba92d51be8
devpapi0891 <rafael50891.dev@gmail.com>
2023-12-16 02:39:09 +0800

    add new property is_protected

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -5,26 +5,31 @@ export * from './assets.utils';
 
 export const primaryLinks = [
 	{
+		is_protected: false,
 		iconifyCode: 'ion:home-sharp',
 		name: 'Home',
 		href: '/'
 	},
 	{
+		is_protected: false,
 		iconifyCode: 'mdi:about-circle-outline',
 		name: 'About',
 		href: '/#about'
 	},
 	{
+		is_protected: false,
 		iconifyCode: 'zondicons:portfolio',
 		name: 'Projects',
 		href: '/#projects'
 	},
 	{
+		is_protected: false,
 		iconifyCode: 'fluent:people-team-20-filled',
 		name: 'Team',
 		href: '/#team'
 	},
 	{
+		is_protected: true,
 		iconifyCode: 'clarity:event-solid',
 		name: 'Events',
 		href: '/#events'
@@ -33,11 +38,13 @@ export const primaryLinks = [
 
 export const secondaryLinks = [
 	{
+		is_protected: true,
 		iconifyCode: 'clarity:event-solid',
 		name: 'Courses',
 		href: '/courses'
 	},
 	{
+		is_protected: false,
 		iconifyCode: 'ic:baseline-phone',
 		name: 'Contact',
 		href: '/contact'
