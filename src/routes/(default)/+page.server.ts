981b93a52a143da1928548410b1d0a3f7099ce0d
devpapi0891 <rafael50891.dev@gmail.com>
2023-12-16 02:43:38 +0800

    stream data for homepage team, projects and events

--- src/routes/(default)/+page.server.ts
+++ src/routes/(default)/+page.server.ts
@@ -1,33 +1,33 @@
 import type { PageServerLoad } from './$types';
 import type { Project, PaginatedTeam } from '@types';
-import { fetchTeam, getAllProjects } from '@server';
+import { fetchTeam, getAllProjects, getAllEvents } from '@server';
 
 export const load: PageServerLoad = async () => {
 	let projects: Project[] = [];
 	let team: PaginatedTeam = {};
+	let events;
 
-	// PROJECTS
-	try {
-		projects = await getAllProjects();
-	} catch (error) {
-		if (error instanceof Error) {
-			console.error(error.message);
-		}
-	}
+	async function fetchPageData() {
+		const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+		await delay(2000);
 
-	// TEAM & USER DETAILS
-	try {
-		const pageNumber = 1;
-		const perPage = 3;
-		team = await fetchTeam(pageNumber, perPage);
-	} catch (error) {
-		if (error instanceof Error) {
-			console.error(error.message);
-		}
+		const [teamData, projectData, eventsData] = await Promise.all([
+			team = await fetchTeam(1, 3),
+			projects = await getAllProjects(),
+			events = await getAllEvents(1, 2)
+		]);
+
+		return {
+			team: teamData,
+			projects: projectData,
+			events: eventsData
+		};
 	}
 
 	return {
-		projects,
-		team
+		test: 'test',
+		streamed: {
+			streamedData: fetchPageData()
+		}
 	};
 };
