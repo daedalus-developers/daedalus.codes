697ebb8ccc1a25408a2d70299867b2d1ab85ced9
Artifex <mac@mkra.dev>
2023-12-17 20:45:27 +0800

    fix: updated query for team

--- src/routes/(default)/+page.server.ts
+++ src/routes/(default)/+page.server.ts
@@ -1,9 +1,9 @@
 import type { PageServerLoad } from './$types';
-import { queryEvents, queryProjects, queryTeam } from '@server/queries';
+import { queryEvents, queryProjects, queryTeamMembers } from '@server/queries';
 
 export const load: PageServerLoad = async () => {
 	return {
-		team: queryTeam(),
+		team: queryTeamMembers(),
 		projects: queryProjects(),
 		events: queryEvents()
 	};
