1243cd08c07098061dbefd6bf044a042103c03c5
Artifex <mac@mkra.dev>
2023-12-17 21:22:32 +0800

    fix: projects grid

--- src/routes/(default)/projects/+page.svelte
+++ src/routes/(default)/projects/+page.svelte
@@ -14,28 +14,34 @@
 	<section class="grid grid-cols-2 gap-2 md:grid-cols-3">
 		{#each projects as project}
 			<!-- TODO: export this to a card component  -->
-			<a href={`/projects/${project.name.toLowerCase().replaceAll(' ', '-')}`}>
-				<div class="card">
-					<header class="card-header mx-auto w-[75%]">
-						<div class="">
-							{#if project.preview}
-								<img src={project.preview} alt={project.title} />
-							{:else}
-								<img src={LOGO_URL} alt={project.title} />
-							{/if}
+			<div class="w-full">
+				<a href={`/projects/${project.name.toLowerCase().replaceAll(' ', '-')}`}>
+					<div class="card rounded-none">
+						<header class="">
+							<div class="items-center">
+								{#if project.preview}
+									<img
+										class="h-full w-full object-cover"
+										src={project.preview}
+										alt={project.title}
+									/>
+								{:else}
+									<img src={LOGO_URL} alt={project.title} />
+								{/if}
+							</div>
+						</header>
+						<div>
+							<section class="space-y-4 p-4">
+								{#each project.tags as tag}
+									<span class="variant-filled chip mx-1">{tag}</span>
+								{/each}
+							</section>
+							<h3 class="text-secondary h3">{project.name}</h3>
 						</div>
-					</header>
-					<div class="mx-auto w-[95%]">
-						<section class="p-4">
-							{#each project.tags as tag}
-								<span class="variant-filled chip mx-1">{tag}</span>
-							{/each}
-						</section>
-						<h3 class="text-primary h3">{project.name}</h3>
+						<footer class="card-footer">{project.description}</footer>
 					</div>
-					<footer class="card-footer">{project.description}</footer>
-				</div>
-			</a>
+				</a>
+			</div>
 		{/each}
 	</section>
 {/await}
