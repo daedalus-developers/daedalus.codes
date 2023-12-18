b878aacc653f0c421c24059aab85ed5b4c2d6a7a
devpapi0891 <rafael50891.dev@gmail.com>
2023-12-18 16:23:07 +0800

    Create projects card create pagination create search field

--- src/routes/(default)/projects/+page.server.ts
+++ src/routes/(default)/projects/+page.server.ts
@@ -1,8 +1,12 @@
 import type { PageServerLoad } from './$types';
 import { queryProjects } from '@server/queries';
 
-export const load: PageServerLoad = () => {
+export const load: PageServerLoad = ({url}) => {
+
+	const page = Number(url.searchParams.get('page')) || 1;
+	const perPage = Math.min(Number(url.searchParams.get('perPage')) || 10, 50);
+
 	return {
-		projects: queryProjects()
+		projects: queryProjects(page, perPage)
 	};
 };
--- src/routes/(default)/projects/+page.svelte
+++ src/routes/(default)/projects/+page.svelte
@@ -1,47 +1,121 @@
 <script lang="ts">
 	import { LOGO_URL } from '@utils';
 	import type { PageData } from './$types';
+	import { page } from '$app/stores';
 	export let data: PageData;
+	
+	$:	queryString = $page.url.searchParams.get('q') || '';
+	$:	currentPerPage = $page.url.searchParams.get('perPage') || '10';
+
 </script>
 
-{#await data.projects}
-	Loading...
-{:then query}
-	<p>Total: {query.totalItems}</p>
-	<p>page: {query.page}</p>
-	<p>PerPage: {query.perPage}</p>
-	{@const projects = query.items}
-	<section class="grid grid-cols-2 gap-2 md:grid-cols-3">
-		{#each projects as project}
-			<!-- TODO: export this to a card component  -->
-			<div class="w-full">
-				<a href={`/projects/${project.name.toLowerCase().replaceAll(' ', '-')}`}>
-					<div class="card rounded-none">
-						<header class="">
-							<div class="items-center">
-								{#if project.preview}
-									<img
-										class="h-full w-full object-cover"
-										src={project.preview}
-										alt={project.title}
-									/>
-								{:else}
-									<img src={LOGO_URL} alt={project.title} />
-								{/if}
-							</div>
-						</header>
-						<div>
-							<section class="space-y-4 p-4">
-								{#each project.tags as tag}
-									<span class="variant-filled chip mx-1">{tag}</span>
-								{/each}
-							</section>
-							<h3 class="text-secondary h3">{project.name}</h3>
+<!-- SEARCH BOX -->
+<div class="py-5 pt-20">
+	<form 
+		action="" 
+		class="flex items-center gap-2">
+		<label class="label w-full">
+			<input 
+				class="input px-4" 
+				type="search" 
+				placeholder="Search here..." />
+		</label>
+		<input type="hidden" class="input" name="perPage">
+		<button class="btn variant-filled flex-none">Search</button>
+	</form>
+</div>
+
+<!-- RESULT ITEMS -->
+<div class="py-10 min-h-[500px]">
+	{#await data.projects}
+		Loading...
+	{:then query}
+		{@const {page, perPage, totalItems, totalPages} = query}
+		{@const itemCount = query.items.length}
+		<div class="pb-5 flex gap-2 opacity-70">
+
+			<p><b>{query.items.length} </b>item{itemCount>1?'s':''} found from page <b>{page}</b> of <b>{totalPages}</b></p>
+		</div>
+		{@const projects = query.items}
+		<section class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+			{#each projects as project, index}
+				<!-- TODO: export this to a card component  -->
+				
+				<a 
+					href={`/projects/${project.name.toLowerCase().replaceAll(' ', '-')}`}
+					title={project.name}
+					class="w-full block card p-2 rounded-lg min-h-[400px]X aspect-[12/15] borderX"
+					>
+					<header class="bg-surface-50-900-token rounded-[inherit] overflow-hidden">
+						<div class="flex items-center justify-center aspect-video overflow-hidden">
+							{#if project.preview}
+								<img
+									class="h-auto max-w-full object-cover rounded-[inherit]"
+									src={project.preview}
+									alt={project.title}
+								/>
+							{:else}
+								<img 
+									class="h-auto max-w-full object-cover rounded-[inherit]"
+									src={LOGO_URL} 
+									alt={project.title} />
+							{/if}
 						</div>
-						<footer class="card-footer">{project.description}</footer>
+					</header>
+					<div class="p-4">
+						<section class="py-1">
+							{#each project.tags as tag}
+								<span class="variant-filled chip mr-1 mb-2">{tag}</span>
+							{/each}
+						</section>
+						<h3 class="text-secondary h4 line-clamp-2 uppercase font-bold">{project.name}</h3>
 					</div>
+					<footer 
+						class="card-footer pb-8"
+						>
+						<div class=" line-clamp-3">
+							{project.description}
+						</div>
+					</footer>
 				</a>
-			</div>
-		{/each}
-	</section>
-{/await}
+			{/each}
+		</section>
+	{/await}
+</div>
+
+
+<!-- PAGINATION -->
+<div class="text-center py-10">
+	
+	{#await data.projects}
+		<!-- Dummy buttons -->
+		<button class="btn variant-filled" disabled={true}>Prev</button>
+		<button class="btn">1</button>
+		<button class="btn variant-filled" disabled={true}>Next</button>
+	{:then query}
+		{@const {page, perPage, totalItems, totalPages} = query}
+
+		{#if page === 1}
+		<button class="btn variant-filled" disabled={true}>Prev</button>
+		{:else}
+		<a 
+			href="/projects?q={queryString}&page={page-1}&perPage={perPage}"
+			class="btn variant-filled">
+			Prev
+		</a>
+		{/if}
+		
+		<button class="btn">{page}</button>
+		
+		{#if page === totalPages}
+		<button class="btn variant-filled" disabled={true}>Next</button>
+		{:else}
+		<a 
+			href="/projects?q={queryString}&page={page+1}&perPage={perPage}"
+			class="btn variant-filled">
+			Next
+		</a>
+		{/if}
+
+	{/await}
+</div>
