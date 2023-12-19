109e0e1d518abb529453105bcec1b421c9d7aa6c
Artifex <mac@mkra.dev>
2023-12-19 12:59:20 +0800

    chore: clean unused query

--- src/lib/server/queries/user.query.ts
+++ src/lib/server/queries/user.query.ts
@@ -1,5 +1,5 @@
 import { db } from '@server';
-import { Collections, type UserRole, type UserWithDetails } from '@types';
+import { Collections } from '@types';
 import type { RecordModel } from 'pocketbase';
 
 export const queryUser = (userId: string) =>
@@ -35,7 +35,9 @@ export const queryUsersByRole = (role: string = '') =>
 		.then((collection) => {
 			collection.forEach((data) => {
 				if (data.expand?.user?.avatar)
-					data.expand.user.avatar = db.files.getUrl(data.expand.user, data.expand.user.avatar);
+					data.expand.user.avatar = db.files.getUrl(data.expand.user, data.expand.user.avatar, {
+						thumb: '300x350'
+					});
 				return data;
 			});
 			return collection;
@@ -54,35 +56,10 @@ export const queryUserByUsername = async (username: string): Promise<RecordModel
 		})
 		.then((data) => {
 			if (data.expand?.user?.avatar)
-				data.expand.user.avatar = db.files.getUrl(data.expand.user, data.expand.user.avatar);
+				data.expand.user.avatar = db.files.getUrl(data.expand.user, data.expand.user.avatar, {
+					thumb: '300x350'
+				});
 			return data;
 		})
 		.catch(() => undefined);
-};
-
-export const queryUserWithDetails = async (id: string): Promise<UserWithDetails | undefined> => {
-	const query = await db
-		.collection(Collections.UsersDetails)
-		.getFirstListItem(db.filter('user = {:id}', { id }), {
-			expand: 'user'
-		});
-	if (!query) return undefined;
-	const userWithDetails: UserWithDetails = {
-		id: query.expand?.user?.id as string,
-		firstName: query.expand?.user?.firstName as string,
-		lastName: query.expand?.user?.lastName as string,
-		email: query.expand?.user?.email as string,
-		username: query.expand?.user?.username as string,
-		role: query.expand?.user?.role as UserRole,
-		verified: query.expand?.user?.validated as boolean,
-		avatar: db.files.getUrl(query.expand?.user, query.expand?.user?.avatar),
-		details: query.details as string,
-		bio: query.bio as string,
-		x: query.x as string,
-		linkedin: query.linkedin as string,
-		github: query.github as string,
-		created: query.created as string,
-		updated: query.updated as string
-	};
-	return userWithDetails;
 };
