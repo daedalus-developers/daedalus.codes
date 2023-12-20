154bfe4f91d926f81b1128bdd97f7c071d887b64
devpapi0891 <rafael50891.dev@gmail.com>
2023-12-21 01:43:03 +0800

    remove unecessary divs fix and match card heights

--- src/lib/components/cards/event-card.svelte
+++ src/lib/components/cards/event-card.svelte
@@ -13,20 +13,29 @@
 	class="block"
 	>
 	<div class="bg-primary-600 hover:bg-secondary-600 rounded-none">
-		<div class="-translate-x-4 -translate-y-4 rounded-[inherit] bg-neutral-50 text-neutral-900">
+		<div class="-translate-x-2 -translate-y-2 rounded-[inherit] 
+			card dark:bg-surface-800-100-token dark:text-surface-100-800-token 
+			border-2 border-zinc-500">
 			<div class="flex justify-center py-4 opacity-60">
 				<Icon icon="ri:calendar-line" class="me-3 text-2xl" />
 				<div class="text-xl uppercase">{type}</div>
 			</div>
 
-			<img src={preview ? preview : `${ASSET_URL}daedalus.png`} alt={title} class="aspect-video object-cover" />
-
+			
+			
+			<div class="aspect-video relative">
+				<img 
+					src={preview ? preview : `${ASSET_URL}daedalus.png`} 
+					alt={title} 
+					class="absolute inset-0 w-full h-full object-cover"
+					 />
+			</div>
+			
 
 			<div class="border-b-2 border-t-2 border-zinc-500 px-4 py-6 text-black">
 				<h1 class="text-ellipsis text-xl font-bold">{title}</h1>
-				<div class="pt-4 text-sm line-clamp-5 min-h-[128px] text-justify" 
-					style="white-space:pre-wrap;">
-					{description}
+				<div class="text-sm min-h-[120px]">
+					<p class="line-clamp-4">{description}</p>
 				</div>
 			</div>
 
