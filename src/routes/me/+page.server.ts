a2f1fd95645e37e5fadd0ce752859710c09b41db
Artifex <mac@mkra.dev>
2023-12-20 09:58:29 +0800

    feat: removed (me) grouped layout

--- src/routes/me/+page.server.ts
+++ src/routes/me/+page.server.ts
@@ -1,7 +1,10 @@
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ parent }) => {
 	const { user } = await parent();
+
+	if (!user) redirect(307, '/login');
 
 	return {
 		user
