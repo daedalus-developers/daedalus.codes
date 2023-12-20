52f1883f122b6afd0d30eb3d7cd980b4a0cf5692
devpapi0891 <rafael50891.dev@gmail.com>
2023-12-21 01:43:46 +0800

    remove unecessary divs add epxport addClass make card background adaptive to theme

--- src/lib/components/cards/project-card.svelte
+++ src/lib/components/cards/project-card.svelte
@@ -3,33 +3,33 @@
 	import type { RecordModel } from 'pocketbase';
 
 	export let project: RecordModel;
-
+	export let addClass: string = '';
 	const { name, description, preview, tags } = project;
 </script>
 
-<a href={`/projects/${name.toLowerCase().replaceAll(' ', '-')}`}>
-	<div class="card rounded-none dark:bg-surface-800-100-token dark:text-surface-100-800-token">
-		<img src={preview ? preview : ASSET_URL + 'daedalus.png'} alt={name} 
-			class="rounded-none aspect-video relative w-full h-full object-cover border-b-2 border-neutral-800"/>
-		<div class="mx-4 flex flex-col gap-1 py-3">
-			<div class="flex flex-row gap-2">
-				{#each tags as tag}
-				<span class="uppercase text-sm opacity-70">{tag}</span>
-				{/each}
-			</div>
-			<div class="min-h-[70px]X">
-				<h3
-				class="text-lg font-bold uppercase tracking-wider sm:text-xl line-clamp-2 text-primary-600"
-			>
-				{name}
-			</h3>
-			</div>
-		</div>
 
-		<div class="card-footer pb-10 min-h-[180px]X">
-			<p class="text-sm sm:text-base line-clamp-3">
-				{description}
-			</p>
+<!-- <div class="card rounded-none dark:bg-surface-800-100-token dark:text-surface-100-800-token"> -->
+<a href={`/projects/${name.toLowerCase().replaceAll(' ', '-')}`}
+	class="card block overflow-hidden {addClass} b-neutral-50">
+	<img 
+		src={preview ? preview : ASSET_URL + 'daedalus.png'} alt={name} 
+		class="rounded-none aspect-[16/9] relative w-full h-full object-cover border-b-2 border-neutral-800"/>
+
+	<div class="p-3">
+		<div class="flex flex-row gap-2">
+			{#if tags.length === 0}
+				<span class="uppercase text-sm opacity-0">No tags</span>
+			{:else}
+				{#each tags as tag}
+					<span class="uppercase text-sm opacity-70">{tag}</span>
+				{/each}
+			{/if}
+		</div>
+		<h3 class="text-xl font-bold mb-3 text-primary-600">{name}</h3>
+		<div class="h-[120px]">
+			<p class="line-clamp-3">{description}</p>
 		</div>
 	</div>
+
 </a>
+
