a9446e78e518b797e561345a72f90e7134d18220
Artifex <mac@mkra.dev>
2023-12-21 17:59:29 +0800

    fix: remove meta tag from layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,17 +20,6 @@
 	});
 </script>
 
-<svelte:head>
-	<title>Daedalus</title>
-	<meta property="og:type" content="article" />
-	<meta property="og:title" content="Daedalus" />
-	<meta
-		property="og:description"
-		content={`We build digital solutions that are both beautiful, functional, efficient and user-friendly.
-				We aim to inspire through our work, leaving a lasting impression on your users.`}
-	/>
-	<meta property="og:image" content={ASSET_URL + 'daedalus.png'} />
-</svelte:head>
 <Drawers />
 <Modal components={ModalRegistry} />
 <Toast position="br" />
