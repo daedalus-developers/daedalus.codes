fc968d4c2d93085b34386dff5bf47b2189d3a58f
devpapi0891 <rafael50891.dev@gmail.com>
2024-03-03 15:56:51 +0800

    Implement Layout conditional redering of Footer Implement Layout conditional redering of Navbar

--- src/routes/(default)/+layout.svelte
+++ src/routes/(default)/+layout.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 	import { Footer, Navbar } from '@components';
 	import { AppShell } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+	$: currentPage = $page.url.pathname;
+	let componentExceptions = ['/login', '/register', '/forgot-password'];
 </script>
 
 <AppShell scrollbarGutter="auto">
 	<svelte:fragment slot="header">
-		<Navbar />
+		{#if !componentExceptions.includes(currentPage)}
+			<Navbar />
+		{/if}
 	</svelte:fragment>
 	<slot />
 	<svelte:fragment slot="pageFooter">
-		<Footer />
+		{#if !componentExceptions.includes(currentPage)}
+			<Footer />
+		{/if}
 	</svelte:fragment>
 </AppShell>
