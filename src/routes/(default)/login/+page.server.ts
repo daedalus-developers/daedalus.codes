26d8e1e9eef0bc07646e08450dfc5e5218577efc
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2024-06-22 00:14:37 +0800

    #203 place await to return block and remove as any typecast

--- src/routes/(default)/login/+page.server.ts
+++ src/routes/(default)/login/+page.server.ts
@@ -3,16 +3,16 @@ import { superValidate } from 'sveltekit-superforms/server';
 import { loginSchema } from '@types';
 import { redirect } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async ({ locals, url }) => {
+export const load: PageServerLoad = async ({ locals }) => {
 	if (locals.DB.authStore.isValid) redirect(307, '/me');
 
 	// Get OAuth methods available
-	const authMethods = await (locals as any).DB.collection('users').listAuthMethods();
-	const fail = url.searchParams.get('fail') === 'true';
-	const {authProviders} = authMethods;
+	// const authMethods = await locals.DB.collection('users').listAuthMethods();
+	// const fail = url.searchParams.get('fail') === 'true';
+	// const { authProviders } = authMethods;
 	// console.log(authProviders);
 	return {
-		authProviders,
+		authProviders: (await locals.DB.collection('users').listAuthMethods())?.authProviders,
 		form: await superValidate(loginSchema)
 	};
 };
