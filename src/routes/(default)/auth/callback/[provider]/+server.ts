ad36a148dcc447e5a67468f23b7d2d8c96e1ddea
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2024-06-23 15:15:19 +0800

    #203 remove unnecessary comments and imports

--- src/routes/(default)/auth/callback/[provider]/+server.ts
+++ src/routes/(default)/auth/callback/[provider]/+server.ts
@@ -1,11 +1,9 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
-import { db } from '@server';
 
 export const GET: RequestHandler = async ({ locals, url, cookies }) => {
 	const provider = JSON.parse(cookies.get('provider') || '{}');
-	// console.log(provider);
 
 	if (provider.state !== url.searchParams.get('state')) {
 		throw new Error("State parameters don't match");
