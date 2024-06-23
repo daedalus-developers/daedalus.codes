e8c0b3300c7a5c9f54ed8f3e79f11e4613d181be
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2024-06-23 16:04:15 +0800

    remove trailing comma

--- src/routes/(default)/+page.server.ts
+++ src/routes/(default)/+page.server.ts
@@ -1,4 +1,3 @@
-import { superValidate } from 'sveltekit-superforms';
 import type { PageServerLoad } from './$types';
 import { queryEvents, queryProjects, queryUsersByRole } from '@server/queries';
 
@@ -6,6 +5,6 @@ export const load: PageServerLoad = async () => {
 	return {
 		team: queryUsersByRole(1, 3, 'team'),
 		projects: queryProjects(1, 3),
-		events: queryEvents(1, 3),
+		events: queryEvents(1, 3)
 	};
 };
