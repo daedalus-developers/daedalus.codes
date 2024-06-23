debfb1a3cdcf70b1fedc996d8d9cf2f30811f124
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2024-06-23 14:51:26 +0800

    #203 redirect no longer requires retturn nor throw. remove locals typecasting as any

--- src/routes/(default)/auth/callback/[provider]/+server.ts
+++ src/routes/(default)/auth/callback/[provider]/+server.ts
@@ -3,26 +3,25 @@ import { redirect } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
 import { db } from '@server';
 
-export const GET: RequestHandler = async ({locals, url, cookies}) => {
-  const provider = JSON.parse(cookies.get('provider') || '{}');
-  // console.log(provider);
+export const GET: RequestHandler = async ({ locals, url, cookies }) => {
+	const provider = JSON.parse(cookies.get('provider') || '{}');
+	// console.log(provider);
 
-  if (provider.state !== url.searchParams.get('state')) {
-      throw new Error('State parameters don\'t match');
-  }
+	if (provider.state !== url.searchParams.get('state')) {
+		throw new Error("State parameters don't match");
+	}
 
-  try {
-    const res = await (locals as any).DB.collection('users').authWithOAuth2Code(
-        provider.name,
-        url.searchParams.get('code') || '',
-        provider.codeVerifier,
-        env.OAUTH_REDIRECT_URL + provider.name
-    )
-  } catch (error) {
-      console.error(JSON.stringify(error));
-      return redirect(303, '/login?fail=true');
-  }
+	try {
+		const res = await locals.DB.collection('users').authWithOAuth2Code(
+			provider.name,
+			url.searchParams.get('code') || '',
+			provider.codeVerifier,
+			env.OAUTH_REDIRECT_URL + provider.name
+		);
+	} catch (error) {
+		console.error(JSON.stringify(error));
+		redirect(303, '/login?fail=true');
+	}
 
-  throw redirect(303, '/login');
+	redirect(303, '/login');
 };
-
